2dcdaca59003b2063390ad07756f5c0d23d20deb
Talento Builder <builder@talento.agency>
2026-01-10 01:50:13 -0500

    fix: Enable Hybrid rendering and Vercel adapter for Keystatic compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
+    output: 'hybrid',
+    adapter: vercel(),
     integrations: [
         tailwind(),
         react(),
