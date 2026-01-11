ea5945f336fdfd662cadbd85d4624c54e56ed27f
Talento Builder <builder@talento.agency>
2026-01-10 20:55:51 -0500

    fix: Brute force injection of ALL keystatic secrets

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -7,9 +7,12 @@ export const prerender = false;
 export const ALL = makeHandler({
     config: {
         ...config,
+        // Inject session secret at runtime
+        secret: process.env.KEYSTATIC_SECRET || config.secret,
         storage: {
             ...config.storage,
-            // @ts-ignore - Inject secret at runtime where process.env follows Node.js conventions
+            // Force injection of GitHub creds at runtime
+            clientId: 'Ov23liEDYhi9gO079O7K',
             clientSecret: config.storage.kind === 'github'
                 ? (process.env.GH_CLIENT_SECRET || process.env.KEYSTATIC_GITHUB_CLIENT_SECRET || config.storage.clientSecret)
                 : config.storage.clientSecret,
